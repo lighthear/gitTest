4e3d501b21def0cfd1051ffddf3147699074adac
lighthear <lighthear@sina.com>
2018-07-11 23:03:50 +0800

    saaaaaaaaaaaaaa

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Hello World!");
         System.out.println("H");
         System.out.println("版本");
-        System.out.println("版本控制");
-        System.out.println("版本控制");
     }
 }
