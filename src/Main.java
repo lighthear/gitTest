9eaf4ae283d8138c2b4b2d73e99b873635ae00c7
lighthear <lighthear@sina.com>
2018-07-11 23:00:28 +0800

    sdasd

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hel");
         System.out.println("Hello World!");
         System.out.println("Hello World!");
+        System.out.println("H");
+        System.out.println("版本");
         System.out.println("版本控制");
         System.out.println("版本控制");
-        System.out.println("版");
     }
 }
