4db0e512d4facc88b5c2f96aaddfc1c585bd85b9
lighthear <lighthear@sina.com>
2018-07-11 22:33:53 +0800

    Merge branch 'master' of https://github.com/lighthear/gitTest
    # Conflicts:
    #	.idea/workspace.xml

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,6 @@ public class Main {
         System.out.println("Hello World!");
         System.out.println("Hello World!");
         System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("版本控制");
         System.out.println("版本控制");
         System.out.println("版本控制");
         System.out.println("版本控制");
