4ee1fde27b34dcdaf7606cf7f156568c78ef8fed
lighthear <lighthear@sina.com>
2018-07-11 21:31:11 +0800

    Revert "add a new line2"
    This reverts commit f94baa454c121ff0b195793f38b248b5317a1b57.
    
    # Conflicts:
    #	.idea/workspace.xml

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("git test is good");
-        System.out.println("版本控制测试");
+        System.out.println("git test!");
     }
 }
