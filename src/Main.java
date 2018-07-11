424bb05dbd21494d0be3acb711ba8e7e3f882c92
lighthear <lighthear@sina.com>
2018-07-11 21:23:42 +0800

    git

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("git test is good");
+        System.out.println("版本控制测试");
     }
 }
