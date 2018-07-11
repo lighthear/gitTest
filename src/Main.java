76396c9ea8254f79a8dd62036f23c429597303f7
lighthear <lighthear@sina.com>
2018-07-11 21:47:31 +0800

    Merge branch 'master' of https://github.com/lighthear/gitTest
    # Conflicts:
    #	.idea/workspace.xml

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("git test!");
-        System.out.println("git test2!");
         System.out.println("版本控制");
     }
 }
